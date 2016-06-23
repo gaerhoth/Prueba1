e1c3fa916fd2f995d97d149ef6c8da62d73179d7
gaerhoth <gaerhoth@gmail.com>
2016-06-23 14:56:50 +0200

    Agregar archivos de proyecto.

---
+++ Prueba1/MainPage.xaml.vb
@@ -0,0 +1,9 @@
+﻿' La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+''' <summary>
+''' Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+End Class
